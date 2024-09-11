684fca2bfa2cdc662fd5ba885ae0f66c5926237f
Stolan <sbelikove@unr.edu>
2024-09-10 20:47:03 -0700

    Add user input in main loop

--- simpleshell.c
+++ simpleshell.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #define PATH_MAX 4096
+#define MAX_LENGTH 100
 
 int main() {
     char cwd[PATH_MAX];
-
     char username[256];
+    char input[MAX_LENGTH];
 
+    // get username
     if (!getlogin_r(username, sizeof(username))) {
         printf("%s:", username);
     }
@@ -18,13 +21,23 @@ int main() {
         return 1;
     }
 
+    // get working directory
     if (getcwd(cwd, sizeof(cwd))) {
-        printf("%s$\n", cwd);
+        printf("%s$ ", cwd);
     }
 
     else {
         perror("getcwd() error");
         return 1;
+    }
+
+    // get user input
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        // Remove trailing newline if present
+        size_t len = strlen(input);
+        if (len > 0 && input[len-1] == '\n') {
+            input[len-1] = '\0';
+        }
     }
 }
 
