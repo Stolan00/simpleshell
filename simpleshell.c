5c1f8f81d7f8c2e780ab566cd7f973be79a46e65
Stolan <sbelikove@unr.edu>
2024-09-11 19:57:02 -0700

    Implement changeDirectories()

--- simpleshell.c
+++ simpleshell.c
@@ -6,55 +6,106 @@
 #define PATH_MAX 4096
 #define MAX_LENGTH 100
 
-int parseInput(char * input, char splitWords[][500], int maxWords);
+int  parseInput(char * input, char splitWords[][500], int maxWords);
+
+void changeDirectories(const char * path);
+
+int is_whitespace(char c) {
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v';
+}
 // --------------------------------------------------------------------------------------------------------------------------
 int main() {
     char cwd[PATH_MAX];
     char username[256];
     char input[MAX_LENGTH];
+    char splitWords[MAX_LENGTH][500];
 
-    // get username
-    if (!getlogin_r(username, sizeof(username))) {
+    while(1) {
+        // Get username
+        if (getlogin_r(username, sizeof(username)) != 0) {
+            perror("getlogin_r");
+            return 1;
+        }
         printf("%s:", username);
-    }
 
-    else {
-        perror("getlogin_r");
-        return 1;
-    }
-
-    // get working directory
-    if (getcwd(cwd, sizeof(cwd))) {
+        // Get working directory
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            perror("getcwd() error");
+            return 1;
+        }
         printf("%s$ ", cwd);
-    }
 
-    else {
-        perror("getcwd() error");
-        return 1;
-    }
-
-    // get user input
-    if (fgets(input, sizeof(input), stdin)) {
-        // Remove trailing newline if present
-        size_t len = strlen(input);
-        if (len > 0 && input[len-1] == '\n') {
-            input[len-1] = '\0';
+        // Get user input
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;  // Exit on EOF
         }
 
-        char splitWords[MAX_LENGTH][500];
+        // Remove trailing newline
+        input[strcspn(input, "\n")] = '\0';
 
-        parseInput(input, splitWords, 10);
+        // Parse input
+        int wordCount = parseInput(input, splitWords, 10);
+
+        // Handle cd command
+        if (wordCount > 0 && strcmp(splitWords[0], "cd") == 0) {
+
+            changeDirectories(splitWords[1]);
+        }
     }
+
+    return 0;
 }
 // --------------------------------------------------------------------------------------------------------------------------
 int parseInput(char * input, char splitWords[][500], int maxWords) {
-    char *token = strtok(input, " ");
     int counter = 0;
-    while (token && counter < maxWords) {
-        strcpy(splitWords[counter], token);
+    char *start = input;
+    char *end;
+    int in_quotes = 0;
+    char quote_char = '\0';
+
+    while (*start && counter < maxWords) {
+        // Skip leading whitespace
+        while (is_whitespace(*start)) start++;
+        if (*start == '\0') break;
+
+        // Check for quotes
+        if (*start == '"' || *start == '\'') {
+            in_quotes = 1;
+            quote_char = *start;
+            start++;
+        }
+
+        // Find end of word or quoted string
+        if (in_quotes) {
+            end = strchr(start, quote_char);
+            if (end == NULL) {
+                // Unterminated quote, treat rest of string as one word
+                end = start + strlen(start);
+            }
+        } else {
+            end = start;
+            while (*end && !is_whitespace(*end)) end++;
+        }
+
+        // Copy word to splitWords
+        strncpy(splitWords[counter], start, end - start);
+        splitWords[counter][end - start] = '\0';
         counter++;
-        token = strtok(NULL, " ");
+
+        // Move start to next word
+        start = (*end == '\0' || in_quotes) ? end : end + 1;
+        in_quotes = 0;
     }
+
     return counter;
+}
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------
+void changeDirectories(const char* path) {
+    if (chdir(path) != 0) {
+        perror("chdir() error");
+    }
 }
 // --------------------------------------------------------------------------------------------------------------------------
